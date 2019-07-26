57b532d64564c00250888b8838529edcaa4b6c35
tanjianqin <tanjianqin@163.com>
2019-07-26 18:01:42 +0800

    完成列表页面

--- web/web-test-1/static/resources/util/config_url.js
+++ web/web-test-1/static/resources/util/config_url.js
@@ -1,8 +1,10 @@
 var domain = "http://localhost:80/";
 var request_type_action = "action";
-var request_type_static = "static";
+var request_type_static = "static/view";
 var model_base = "base";
 var url_base_user_selectPager = domain + request_type_action + "/"+ model_base  + "/user/selectPager";
+var url_base_user_delete = domain + request_type_action + "/"+ model_base  + "/user/delete";
+var url_base_user_edit = domain + request_type_static + "/"+ model_base  + "/user/user_edit.html";
 
 var model_mall = "mall";
 var url_mall_goods_selectPager = domain + request_type_action + "/"+ model_mall  + "/goods/selectPager";
