b8b7857abaf93eb14bc506150e42bd6ec0af1925
tanjianqin <tanjianqin@163.com>
2019-07-31 01:19:25 +0800

    改造成微服务

--- web/web-test-1/static/resources/util/config_url.js
+++ web/web-test-1/static/resources/util/config_url.js
@@ -5,6 +5,9 @@ var model_base = "base";
 var url_base_user_selectPager = domain + request_type_action + "/"+ model_base  + "/user/selectPager";
 var url_base_user_delete = domain + request_type_action + "/"+ model_base  + "/user/delete";
 var url_base_user_edit = domain + request_type_static + "/"+ model_base  + "/user/user_edit.html";
+var url_base_user_selectOne = domain + request_type_action + "/"+ model_base  + "/user/selectOne";
+
+var url_base_code_selectList = domain + request_type_action + "/"+ model_base  + "/code/selectList";
 
 var model_mall = "mall";
 var url_mall_goods_selectPager = domain + request_type_action + "/"+ model_mall  + "/goods/selectPager";
